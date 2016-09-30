53398905f3a04541e66136757bec46e1cecc1a56
Gabe Fierro <gtfierro225@gmail.com>
2016-09-30 11:05:55 -0700

    drastically simplify the worker pattern

--- common/workerpool.go
+++ common/workerpool.go
@@ -4,59 +4,31 @@ import (
 	bw "gopkg.in/immesys/bw2bind.v5"
 )
 
-type worker struct {
-	cb func(msg *bw.SimpleMessage)
-}
-
-func newWorker(cb func(msg *bw.SimpleMessage)) *worker {
-	w := &worker{
-		cb: cb,
+func worker(in chan *bw.SimpleMessage, cb func(msg *bw.SimpleMessage)) {
+	for msg := range in {
+		cb(msg)
 	}
-	return w
-}
-
-func (w *worker) run(msg *bw.SimpleMessage) {
-	w.cb(msg)
 }
 
 type workerPool struct {
-	workers chan *worker
-	in      chan *bw.SimpleMessage
-	cb      func(msg *bw.SimpleMessage)
+	in   chan *bw.SimpleMessage
+	cb   func(msg *bw.SimpleMessage)
+	size int
 }
 
 func NewWorkerPool(in chan *bw.SimpleMessage, cb func(msg *bw.SimpleMessage), size int) *workerPool {
 	pool := &workerPool{
-		workers: make(chan *worker, size),
-		in:      in,
-		cb:      cb,
+		in:   in,
+		cb:   cb,
+		size: size,
 	}
-	for i := 0; i < size; i++ {
-		pool.workers <- newWorker(pool.cb)
-	}
-
 	return pool
 }
 
 func (pool *workerPool) Start() {
 	go func() {
 		for msg := range pool.in {
-			select {
-			case worker := <-pool.workers:
-				go pool.handle(worker, msg)
-			default:
-				worker := newWorker(pool.cb)
-				go pool.handle(worker, msg)
-			}
+			go pool.cb(msg)
 		}
 	}()
-}
-
-func (pool *workerPool) handle(dude *worker, msg *bw.SimpleMessage) {
-	dude.run(msg)
-	select {
-	case pool.workers <- dude:
-	default:
-		dude = nil
-	}
 }
