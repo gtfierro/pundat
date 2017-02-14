5f7341736eec26edc6da8dec224952d6dcf38fc8
Gabe Fierro <gtfierro225@gmail.com>
2017-02-14 09:39:36 -0800

    add example

--- client/client.go
+++ client/client.go
@@ -1,3 +1,33 @@
+// This package provides a simple barebones client to the pundat archiver
+// Usage:
+//
+//    package main
+//
+//    import (
+//    	"fmt"
+//    	"log"
+//    	"os"
+//
+//    	"github.com/gtfierro/pundat/client"
+//    	bw "gopkg.in/immesys/bw2bind.v5"
+//    )
+//
+//    func main() {
+//    	bwclient := bw.ConnectOrExit("")
+//    	bwclient.OverrideAutoChainTo(true)
+//    	vk := bwclient.SetEntityFromEnvironOrExit()
+//
+//    	pc, err := client.NewPundatClient(bwclient, vk, "ucberkeley")
+//    	if err != nil {
+//    		log.Fatal(err)
+//    	}
+//    	md, ts, ch, err := pc.Query(os.Args[1], 10)
+//
+//    	fmt.Println("md", md)
+//    	fmt.Println("ts", ts)
+//    	fmt.Println("ch", ch)
+//    	fmt.Println("err", err)
+//    }
 package client
 
 import (
